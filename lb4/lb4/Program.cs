5b0004cb6213204c3343c825202220c55ed366f8
DanilaFilippov <54959397+DanilaFilippov@users.noreply.github.com>
2026-04-18 21:14:07 +0300

    Add files via upload

---
+++ lb4/lb4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader("input.txt"));
+            Console.SetOut(new StreamWriter("output.txt") { AutoFlush = true });
+        
+            int n = int.Parse(Console.ReadLine());
+            string line = Console.ReadLine();
+
+            double[] arr = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+            // ot 1000 do 5000
+            double sumR = arr.Where(x => x > 1000 && x < 5000).Sum();
+            // min max
+            double sumMM = arr.Max() + arr.Min();
+            // mod <= 1000
+            var res = arr.Where(x => Math.Abs(x) <= 1000);
+
+            Console.WriteLine($"1) Сумма в диапазоне (1000, 5000): {sumR}");
+            Console.WriteLine($"2) Сумма Max + Min: {sumMM}");
+            Console.Write("3) Элементы |x| <= 1000: ");
+            Console.WriteLine(string.Join("; ", res.Select(x => x.ToString("F2"))));
+        }
+    }
+}
---
+++ lb4/lb4/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("lb4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lb4")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("17771b71-d14f-4478-86bd-f6662c092546")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
