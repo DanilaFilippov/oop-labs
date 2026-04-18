aecbba25989438f227f9064b02312f1b281ebb61
DanilaFilippov <54959397+DanilaFilippov@users.noreply.github.com>
2026-04-18 21:15:06 +0300

    Add files via upload

---
+++ Lb3/Lb3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lb3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Введите X: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Введите Y: ");
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("Введите N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            double z = 1.0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+
+                double num;
+                if (i == 1)
+                    num = 1;
+                else if (i % 2 == 0)
+                    num = Math.Pow(x, i - 1);
+                else
+                    num = Math.Pow(y, i - 1);
+
+                double tmp = num / factorial;
+                if (i % 2 == 0)
+                    z -= tmp;
+                else
+                    z += tmp;
+            }
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Результат Z = {0:F6}", z);
+            
+            Console.WriteLine("\nНажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
